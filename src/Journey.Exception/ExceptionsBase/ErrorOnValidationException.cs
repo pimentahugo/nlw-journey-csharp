efbddc5c9f59fbff3fb3c4a11c6cae163a1b1629
pimentahugo <hugopimenta_marinho_machado@hotmail.com>
2024-07-14 16:55:45 -0300

    feat: 2° aula nlw journey c-sharp

---
+++ src/Journey.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Journey.Exception.ExceptionsBase;
+public class ErrorOnValidationException : JourneyException
+{
+	private readonly IList<string> _errors;
+
+	public ErrorOnValidationException(IList<string> message) : base(string.Empty)
+	{
+		_errors = message;
+	}
+
+	public override IList<string> GetErrorsMessage()
+	{
+		return _errors;
+	}
+
+	public override HttpStatusCode GetStatusCode()
+	{
+		return HttpStatusCode.BadRequest;
+	}
+}
---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure;
+public class JourneyDbContext : DbContext
+{
+
+	public DbSet<Trip> Trips { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlite("Data Source=C:\\Users\\hugop\\source\\repos\\Journey\\JourneyDatabase.db");
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<Activity>().ToTable("Activities");
+	}
+
+}
+
